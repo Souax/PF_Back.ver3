d7967b5e0a69a5ec4086a297d899dcbf6fba90a1
Souax <126418531+Souax@users.noreply.github.com>
2024-04-14 16:44:14 +0000

    ドメイン

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'http://www.newbookers.com'
+     origins 'https://pf-front-ver3-soutas-projects.vercel.app'
 
      resource '*',
        headers: :any,
